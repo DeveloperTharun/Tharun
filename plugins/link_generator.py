f80825a49aa9c8886af77d58431927ce5fc0eb4c
Logesh <logesh.12a3@gmail.com>
2024-03-16 17:42:41 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -6,6 +6,19 @@ from bot import Bot
 from config import ADMINS
 from helper_func import encode, get_message_id
 
+def human_readable_size(size):
+    if size < 1024:
+        return f"{size} Bytes"
+    size /= 1024.0
+    if size < 1024:
+        return f"{size:.2f} KB"
+    size /= 1024.0
+    if size < 1024:
+        return f"{size:.2f} MB"
+    size /= 1024.0
+    return f"{size:.2f} GB"
+
+
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
 async def batch(client: Client, message: Message):
     while True:
@@ -44,17 +57,33 @@ async def batch(client: Client, message: Message):
 async def link_generator(client: Client, message: Message):
     while True:
         try:
-            channel_message = await client.ask(text = "Forward Message from the DB Channel (with Quotes)..\nor Send the DB Channel Post link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            channel_message = await client.ask(text="Forward Message from the DB Channel (with Quotes)..\nor Send the DB Channel Post link", chat_id=message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
         msg_id = await get_message_id(client, channel_message)
         if msg_id:
             break
         else:
-            await channel_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is not taken from DB Channel", quote = True)
+            await channel_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is not taken from DB Channel", quote=True)
             continue
+
+    forward_message = channel_message.forward_from_message_id or channel_message
+    file_size = None
+
+    if forward_message.document:
+        file_size = forward_message.document.file_size
+    elif forward_message.audio:
+        file_size = forward_message.audio.file_size
+    elif forward_message.video:
+        file_size = forward_message.video.file_size
 
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
     link = f"https://filestore.rapidbots.workers.dev?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await channel_message.reply_text(f"<b>Here is your link</b>\n\n<code>{link}</code>", quote=True, reply_markup=reply_markup)
+
+    reply_message = f"<b>Here is your link</b>\n\n<code>{link}</code>"
+    if file_size:
+        file_size_readable = human_readable_size(file_size)
+        reply_message += f"\n\nFile Size: {file_size_readable}"
+
+    await channel_message.reply_text(reply_message, quote=True, reply_markup=reply_markup)
