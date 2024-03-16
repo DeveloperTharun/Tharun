ccfbe11187dba7a25a392145e9e65fabcbdcfd0a
Logesh <logesh.12a3@gmail.com>
2024-03-16 15:48:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
+DB_NAME = os.environ.get("DATABASE_NAME", "fileshareexbot")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = lambda: int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
