a22306b5215fbe475edc8cb9d02b4ac751e6be89
DeveloperTharun <114932079+DeveloperTharun@users.noreply.github.com>
2024-04-04 06:37:40 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -70,7 +70,7 @@ async def channel_post(client: Client, message: Message):
 
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
 
-    await reply_text.edit(f"🌫 <a href='{link}'>{previouscaptions}</a>", reply_markup=reply_markup, disable_web_page_preview = True)
+    await reply_text.edit(f"🌫 <a href='{link}'>{previouscaptions} 04</a>", reply_markup=reply_markup, disable_web_page_preview = True)
 
     if not DISABLE_CHANNEL_BUTTON:
         await post_message.edit_reply_markup(reply_markup)
